86330f3160a91f0f9fc520d566e8ae34a6568cef
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-17 22:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices=document.getElementsByClassName("price");
+	let ans=0;
+	for(let i of prices){
+		ans+=Number(i.innerText);
+	}
+	// console.log(ans);
+	let row=document.createElement("tr");
+	let col1=document.createElement("td");
+	let col2=document.createElement("td");
+	col2.id="ans";
+	col2.innerText=`${ans}`;
+	row.appendChild(col1);
+	row.appendChild(col2);
+	let table=document.getElementsByTagName("table")[0];
+	table.appendChild(row);
   
 };
 
